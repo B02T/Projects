e34ab088a0e4cc79f1220df404ff7458d93d27fe
B02T <boian1@abv.bg>
2021-01-21 13:01:30 +0100

    When my quarantine ends 27 Jan 2021

---
+++ script.js
@@ -0,0 +1,21 @@
+"use struct";
+const d = document.getElementById("d");
+const h = document.getElementById("h");
+const m = document.getElementById("m");
+const s = document.getElementById("s");
+
+function countdown() {
+  const currentDate = new Date();
+  const departureDate = new Date("27 Jan 2021");
+  const differanceInSeconds = (departureDate - currentDate) / 1000;
+  const days = Math.floor(differanceInSeconds / 3600 / 24);
+  const hours = Math.floor(differanceInSeconds / 3600) % 24;
+  const min = Math.floor(differanceInSeconds / 60) % 60;
+  const sec = Math.floor(differanceInSeconds) % 60;
+  d.innerHTML = days;
+  h.innerHTML = hours;
+  m.innerHTML = min;
+  s.innerHTML = sec;
+}
+
+setInterval(countdown, 1000);

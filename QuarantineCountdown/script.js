0489179ead83a057e39e7f398ce2ed45eadf4f98
B02T <boian1@abv.bg>
2021-02-08 17:58:30 +0100

    ToDoList ready

--- QuarantineCountdown/script.js
+++ QuarantineCountdown/script.js
@@ -4,16 +4,15 @@ const h = document.getElementById("h");
 const m = document.getElementById("m");
 const s = document.getElementById("s");
 
-
 function countdown() {
   const currentDate = new Date();
   const departureDate = new Date("27 Jan 2021");
 
   if (currentDate > departureDate) {
-    departureDate.setDate(departureDate.getDate() + 10)
+    departureDate.setDate(departureDate.getDate() + 10);
   }
 
-  const differanceInSeconds = (departureDate - currentDate) / 1000;
+  const differanceInSeconds = Math.abs(departureDate - currentDate) / 1000;
   const days = Math.floor(differanceInSeconds / 3600 / 24);
   const hours = Math.floor(differanceInSeconds / 3600) % 24;
   const min = Math.floor(differanceInSeconds / 60) % 60;
@@ -25,4 +24,3 @@ function countdown() {
 }
 
 setInterval(countdown, 1000);
-
--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,10 +1,19 @@
-const btnAddToDo = document.getElementById('#addToDo');
-const toDoContainer = document.getElementById('#toDoContainer');
-const inputField = document.getElementById('#inputField');
+let btn = document.querySelector('#btn');
+const toDoContainer = document.getElementById('toDoContainer');
+const inputField = document.getElementById('inputField');
 
-btnAddToDo.addEventListener('click', () => {
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling')
+btn.addEventListener("click", function() {
+    console.log('click');
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph')
     paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph)
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+
+    });
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    });
 });

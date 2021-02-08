f02a7bcf2c49ed09ab5171722650f8efabf9536c
B02T <boian1@abv.bg>
2021-02-08 21:06:25 +0100

    simple JumpGame alpha 1.0.0 'TODO jump loop'

---
+++ SimpleJumpGame/script.js
@@ -0,0 +1,11 @@
+const char = document.querySelector('#character');
+const block = document.querySelector('#block'); 
+const game = document.querySelector('.game');
+function jump(){
+    char.classList.add('animate');  
+
+}
+
+game.addEventListener('click',() => {
+  
+});
